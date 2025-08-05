8251459c48a5e1a9f8e15451a2b16507c9feab6f
999geo <juice999geo@gmail.com>
2025-08-06 00:04:17 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+window.location.href = "/judgment.html"
+  ;document.getElementById("unlockBtn").addEventListener("click", async () => {
+  const response = await fetch("/verify-token", {
+    method: "POST",
+    body: JSON.stringify({ userId: "..." }),
+    headers: { "Content-Type": "application/json" }
+  });
+
+  const result = await response.json();
+  if (result.success) {
+    document.getElementById("villainPanels").style.display = "block";
+  } else {
+    alert("Access denied. Try again.");
+  }
+});
